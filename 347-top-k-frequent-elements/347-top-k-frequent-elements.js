140c7b4bdd2040b4cf7da4c8a05caccaf3749fa3
iamitpatil <51691510+iamitpatil@users.noreply.github.com>
2022-06-25 23:52:59 +0530

    Time: 398 ms (5.04%), Space: 51 MB (7.53%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = new Map();
+    for(let i=0; i<nums.length; i++) {
+        map.has(nums[i]) ?
+            map.set(nums[i], map.get(nums[i])+1):
+            map.set(nums[i], 1);
+    }
+    
+    let array = new Array(nums.length);
+    for(const [key, value] of map.entries()) {
+        array[value-1] ?
+            array[value-1].push(key):
+            (array[value-1]=[key]);
+    }
+    console.log(map)
+    let result=[], j=array.length-1;
+    while(result.length < k) {
+        console.log(array[j])
+        array[j] ? result.push(...array[j]): undefined;
+        j = j-1;
+    }
+    return result;
+};
