b9ed0e8be4ffc6398e886a96602902e6cc5a2b33
iamitpatil <51691510+iamitpatil@users.noreply.github.com>
2022-06-27 10:08:30 +0530

    Time: 698 ms (15.67%), Space: 58 MB (40.10%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let map = new Set();
+    let set = new Set(nums);
     let longest = 0;
     for(let i=0; i<nums.length; i++) {
-        map.add(nums[i]);
-    }
-    for(let i=0; i<nums.length; i++) {
         let strlen = 0;
-        if(!map.has(nums[i]-1)) {
-            while(map.has(nums[i]+strlen)) {
+        if(!set.has(nums[i]-1)) {
+            while(set.has(nums[i]+strlen)) {
                 strlen++;
             }
             longest = strlen > longest ? strlen: longest;
