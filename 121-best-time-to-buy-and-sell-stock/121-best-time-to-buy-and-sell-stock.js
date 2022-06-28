c4de95846df57d0321fc5cc8c01e75a52e9ba4a0
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-06-28 21:04:00 +0530

    Time: 164 ms (9.77%), Space: 51.8 MB (53.09%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -6,9 +6,7 @@ var maxProfit = function(prices) {
     let profit = 0, min = Infinity;
     for(let i=0; i<prices.length; i++) {
         min = Math.min(min, prices[i]);
-        if(prices[i]-min > profit) {
-            profit = prices[i]-min;
-        }
+        profit = Math.max(profit, prices[i]-min)
     }
     return profit;
 };
