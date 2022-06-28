4ff06592faf2b841ad4eb9beecfdf549cb27d94b
iamitpatil <51691510+iamitpatil@users.noreply.github.com>
2022-06-28 09:34:09 +0530

    Time: 241 ms (40.93%), Space: 53.1 MB (36.48%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let array = [];
+    nums.sort((a,b) => a-b);
+    for(let i=0; i<nums.length; i++) {
+        if(i>0 && nums[i] == nums[i-1]) {
+            continue;
+        }
+        let start = i+1;
+        let end = nums.length-1;
+        while(start<end) {
+            let sum = nums[i] + nums[start] + nums[end];
+            if(sum > 0) {
+                end--;
+            }
+            else if(sum < 0) {
+                start++;
+            }
+            else {
+                array.push([nums[i], nums[start], nums[end]]);
+                start++;
+                while((nums[start] == nums[start-1]) && (start< end)) {
+                    start++;
+                } 
+            }
+        }
+    }
+    return array;
+};
