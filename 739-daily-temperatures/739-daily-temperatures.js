3671206a7f5f6361c5540dfea40d621c86a5856e
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-04 00:33:47 +0530

    Time: 669 ms (27.74%), Space: 63.1 MB (77.65%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -6,7 +6,8 @@ var dailyTemperatures = function(temperatures) {
     let result = new Array(temperatures.length).fill(0), stack = [];
     for(let i=0; i<temperatures.length; i++) {
         while(stack.length && temperatures[stack.at(-1)] < temperatures[i]) {
-            result[stack.at(-1)] = i-stack.pop();
+            result[stack.at(-1)] = i-stack.at(-1);
+            stack.pop();
         }
         stack.push(i);
     }
