6602a521aa8a0208e9bdebcbc63ef0937a77557a
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-03 20:22:08 +0530

    Time: 140 ms (8.11%), Space: 42.3 MB (91.54%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = [];
+    let str = "";
+    let backtracking = function(str = "", open = 0, close = 0) {
+        if(open == close && close == n)
+            result.push(str);
+        if(open < n) {
+            backtracking(str + "(", open + 1, close);
+        }
+        if(close < open) {
+            backtracking(str + ")", open, close + 1);
+        }
+    }
+    backtracking();
+    return result;
+};
