90ed692a322228ff9aef5eb5013e25bfdf64c8bf
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-04 14:25:28 +0530

    Time: 367 ms (13.12%), Space: 75.1 MB (15.22%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.js
@@ -6,12 +6,10 @@ var largestRectangleArea = function(heights) {
     let max = 0, stack = [];
     for(let i=0; i<heights.length; i++) {
         let current = [i, heights[i]];
-        if(stack.length && heights[i]<stack.at(-1)[1]) {
-            while(stack.length && stack.at(-1)[1] > heights[i]) {
-                let top = stack.at(-1);
-                max = Math.max(max, (i-top[0])*top[1], (i-top[0]+1)*heights[i]);
-                current[0] = stack.pop()[0];
-            }
+        while(stack.length && stack.at(-1)[1] > heights[i]) {
+            let top = stack.at(-1);
+            max = Math.max(max, (i-top[0])*top[1], (i-top[0]+1)*heights[i]);
+            current[0] = stack.pop()[0];
         }
         stack.push(current);
     }
