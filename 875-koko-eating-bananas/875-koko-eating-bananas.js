d007bb0572e688fc66618213926bc33e74bfc435
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-04 17:48:20 +0530

    Time: 281 ms (5.20%), Space: 48.9 MB (6.50%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let kr = 0, kl = 1, k=Number.POSITIVE_INFINITY;
+    piles.forEach((len) => kr = Math.max(kr, len));
+    let isPossible = (k) => {
+        return h >= piles.map((item) => Math.ceil(item/k)).reduce((sum, a) => sum+a, 0);
+    }
+    while(kr>=kl) {
+        mid = kl + Math.floor((kr-kl)/2);
+        if(isPossible(mid)) {
+            k=mid;
+            kr = mid-1;
+        } else {
+            kl = mid+1;
+        }
+    }
+    return k;
+};
