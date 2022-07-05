e31f9dc73d7e75bd79ba05646de3accb379e21c9
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-05 13:53:08 +0530

    Time: 145 ms (68.37%), Space: 46 MB (94.79%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,21 +4,19 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let len1 = nums1.length;
-    let len2 = nums2.length;
+    let len1 = nums1.length, len2 = nums2.length;
     if(len1>len2) return findMedianSortedArrays(nums2, nums1);
-    let totalSize = (len1 + len2);
-    let start = 0, end = len1;
+    let totalSize = (len1 + len2), start = 0, end = len1;
     while(start <= end) {
     
         let cut1 = (end+start)>>1;
         let cut2 = Math.floor((len1+len2+1)/2) - cut1;
         
-        let left1 = cut1 == 0 ? Number.MIN_SAFE_INTEGER: nums1[cut1-1];
-        let left2 = cut2 == 0 ? Number.MIN_SAFE_INTEGER: nums2[cut2-1];
+        let left1 = cut1 == 0 ? Number.NEGATIVE_INFINITY: nums1[cut1-1];
+        let left2 = cut2 == 0 ? Number.NEGATIVE_INFINITY: nums2[cut2-1];
         
-        let right1 = cut1 == len1 ? Number.MAX_SAFE_INTEGER: nums1[cut1];
-        let right2 = cut2 == len2 ? Number.MAX_SAFE_INTEGER: nums2[cut2];
+        let right1 = cut1 == len1 ? Number.POSITIVE_INFINITY: nums1[cut1];
+        let right2 = cut2 == len2 ? Number.POSITIVE_INFINITY: nums2[cut2];
         
         if(left2 <= right1 && right2 >= left1) {
             if(((len1+len2)%2) == 0 )
