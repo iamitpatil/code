27521d1fb6eed47e918791285bbdc649f04142f1
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-05 17:37:21 +0530

    Time: 110 ms (23.66%), Space: 44.6 MB (15.59%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -9,13 +9,28 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+//with iteration
 var reverseList = function(head) {
-    if(head == null) return null;
-    if(head.next == null)  return head;
-    else {
-        let reversedHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return reversedHead;
+    let prev = null, current = head, next;
+    while(current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
     }
+    return prev;
 };
+
+
+//recuresive solution
+// var reverseList = function(head) {
+//     if(head == null) return null;
+//     if(head.next == null)  return head;
+//     else {
+//         let reversedHead = reverseList(head.next);
+//         head.next.next = head;
+//         head.next = null;
+//         return reversedHead;
+//     }
+// };
