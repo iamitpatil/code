8f19c9831279011c8dd2dc67c764ba2bc0f2be57
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-06 23:21:22 +0530

    Time: 153 ms (60.99%), Space: 46.7 MB (95.50%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -9,18 +9,54 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    let array = [];
-    for(let i=0; i<lists.length; i++) {
-        let list = lists[i];
-        while(list != null) {
-            array.push(list);
-            list = list.next;
+var mergeLists = function(l1, l2) {
+    let head = new ListNode(), list = head;
+    while(l1 != null || l2 != null) {
+        if(l1 != null && l2!=null) {
+            if(l1.val >= l2.val) {
+                head.next = l2;
+                l2 = l2.next;
+            } else {
+                head.next = l1;
+                l1 = l1.next;
+            }
+        } else if(l1 != null) {
+            head.next = l1;
+            break;
+        } else {
+            head.next = l2;
+            break;
         }
+        head = head.next;
     }
-    array.sort((a,b) => a.val - b.val);
-    for(let i=0; i<array.length-1; i++) {
-        array[i].next = array[i+1];
+    return list.next;
+}
+var mergeKLists = function(lists) {
+    let mergedLists;
+    while(lists.length > 1) {
+        mergedLists = [];
+        for(let i=0; i<lists.length; i=i+2) {
+            mergedLists.push(mergeLists(lists[i], (i+1<lists.length) ? lists[i+1]: null));
+        }
+        lists = mergedLists;
     }
-    return array.length ? array[0]: null;
+    return (lists.length == 1) ? lists[0]: null;
 };
+
+
+// heapsort O((n.k) Log(n.k)) k= avg length of each list, n = lists len
+// var mergeKLists = function(lists) {
+//     let array = [];
+//     for(let i=0; i<lists.length; i++) {
+//         let list = lists[i];
+//         while(list != null) {
+//             array.push(list);
+//             list = list.next;
+//         }
+//     }
+//     array.sort((a,b) => a.val - b.val);
+//     for(let i=0; i<array.length-1; i++) {
+//         array[i].next = array[i+1];
+//     }
+//     return array.length ? array[0]: null;
+// };
