9b28002bec2c4d66573950d6201794f84a907433
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-07 22:53:04 +0530

    Time: 159 ms (12.18%), Space: 51.5 MB (61.39%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
@@ -16,7 +16,11 @@ var maxPathSum = function(root) {
         if(node == null) return 0;
         let left = sum(node.left);
         let right = sum(node.right);
-        total = Math.max(total, left + right + node.val, left+node.val, right+node.val, node.val);
+        total = Math.max(total, 
+                left + right + node.val, 
+                left+node.val, 
+                right+node.val, 
+                node.val);
         return Math.max(left+node.val, right+node.val, node.val);
     };
     return Math.max(sum(root), total);
