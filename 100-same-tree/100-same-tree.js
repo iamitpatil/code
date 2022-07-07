1b0e00abc7e5bd330c7036f730ee0e988a4497c7
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-07 12:48:39 +0530

    Time: 118 ms (6.59%), Space: 42.1 MB (71.04%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,9 +12,7 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if(p==null && q==null) return true;
-    if(p==null && q!=null) return false;
-    if(p!=null && q==null) return false;
+    if(p==null || q==null) return (p==q);
     if(p.val != q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
