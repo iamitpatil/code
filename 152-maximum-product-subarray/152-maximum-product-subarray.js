2785ecb8b838b788518a2ba640ce530aa20d55f1
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-15 17:42:24 +0530

    Time: 63 ms (97.79%), Space: 42.9 MB (43.68%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -5,11 +5,6 @@
 var maxProduct = function(nums) {
     let max=1, min=1, temp, res=Math.min(...nums);
     for(let i=0; i<nums.length; i++) {
-        if(nums[i] == 0) {
-            res = Math.max(nums[i], res);
-            max=1, min=1;
-            continue;
-        }
         temp = max*nums[i];
         max = Math.max(temp, nums[i]*min, nums[i]);
         min = Math.min(temp, nums[i]*min, nums[i]);
