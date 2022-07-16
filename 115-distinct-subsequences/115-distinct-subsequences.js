61a11f5bec97e2e998f734bf52eb3e227b4e0369
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-17 00:30:58 +0530

    Time: 1063 ms (17.70%), Space: 143.6 MB (9.57%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var numDistinct = function(s, t) {
+    let dp = new Map();
+    let dfs = (i, j) => {
+        if(i>=s.length && j>=t.length) return 1;
+        else if(i>=s.length) return 0;
+        if(dp.has(`${i}.${j}`)) return dp.get(`${i}.${j}`);
+        let res;
+        if(s[i]==t[j]) {
+            res = dfs(i+1, j+1) + dfs(i+1, j);
+        } else {
+            res = dfs(i+1, j);
+        }
+        dp.set(`${i}.${j}`, res);
+        return res;
+    };
+    return dfs(0, 0);
+};
