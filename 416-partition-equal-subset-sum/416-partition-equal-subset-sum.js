8be46c76b195d1edb62bf05a4f2d1fcd5e0f4523
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-16 12:51:00 +0530

    Time: 352 ms (46.92%), Space: 53.3 MB (55.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -7,18 +7,15 @@ var canPartition = function(nums) {
     nums.forEach((i) => sum=sum+i);
     if(sum%2 == 1) return false;
     sum = sum/2;
-    let total=0;
-    let dp = new Array(nums.length).fill(0).map(()=> new Array(sum+1).fill(0));
-    dp.forEach((i)=> i[0]=1);
-    for(let total=0; total<=sum; total++) {
-        for(let i=nums.length-1; i>=0; i--) {
-            if(i==nums.length-1) {
-                if(nums[i]==total) dp[i][total] = 1;
-            } else if(i<nums.length-1){
-                if(total>=nums[i]) dp[i][total] = dp[i+1][total-nums[i]] || dp[i+1][total];
-                else dp[i][total] = dp[i+1][total];
-            }
+    let dp = new Set([0]);
+    for(let i=nums.length-1; i>=0; i--) {
+        let temp = new Set();
+        for(let val of dp) {
+            if(val+nums[i]==sum) return true;
+            temp.add(val+nums[i]);
+            temp.add(val);
         }
+        dp = temp;
     }
-    return dp[0][sum] ? true: false;
+    return dp.has(sum) ? true: false;
 };
