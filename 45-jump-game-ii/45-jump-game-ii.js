1bee7442b2d14a792c0cdefbb2294a60ff71be0f
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-17 14:24:43 +0530

    Time: 120 ms (48.66%), Space: 44 MB (71.97%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -4,7 +4,7 @@
  */
 var jump = function(nums) {
     let l =0, r = 0, currMax = 0, jumps=0;
-    while(r>=l && r < nums.length-1) {
+    while(r < nums.length-1) {
         currMax = 0;
         for(let i=l ; i<r+1; i++) currMax = Math.max(currMax, i+ nums[i]);
         l=r+1;
