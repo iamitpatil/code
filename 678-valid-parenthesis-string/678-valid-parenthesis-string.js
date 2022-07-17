daee1040a2f12da180b350056929237df5b59c79
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-17 19:30:55 +0530

    Time: 104 ms (22.01%), Space: 42.3 MB (28.93%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    let stack = [], lmin=0, lmax=0;
+    let lmin=0, lmax=0;
     for(let i=0; i<s.length; i++) {
         if(s[i]=='(') lmin++, lmax++;
         else if(s[i] == ')') lmin--,lmax--;
