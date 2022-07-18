fdd43222198e10b3360a875787f985ba9431fdc5
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2022-07-18 08:00:47 +0530

    Time: 104 ms (95.82%), Space: 55.5 MB (34.09%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -5,13 +5,12 @@
 var productExceptSelf = function(nums) {
     let answer = new Array(nums.length).fill(1);
     let right = 1;
-    let answerR = new Array(nums.length).fill(1);
     for(let i=1; i<nums.length; i++) {
         answer[i] = answer[i-1]*nums[i-1];
     }
-    for(let i=nums.length-2; i>=0; i--) {
-        answer[i] = right*nums[i+1]*answer[i];
-        right = right*nums[i+1];
+    for(let i=nums.length-1; i>=0; i--) {
+        answer[i] = right*answer[i];
+        right = right*nums[i];
     }
     return answer;
 };
