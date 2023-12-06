3ad31ab86aef6b59927ecd67381ee56ee0e9dfc9
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2023-12-06 16:31:20 +0530

    Time: 503 ms (18.55%), Space: 147.3 MB (14.46%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.js
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.js
@@ -29,5 +29,28 @@ var maxProfit = function(prices) {
         dp[i][isHeld][trx] = result;
         return result;
     }
-    return findMaxProfitWithDP(0, 0, 2);
+    let findWithDivideAndConquer = () => {
+        let left = new Array(prices.length).fill(null);
+        let right = new Array(prices.length).fill(null);
+        let lmin = Number.MAX_VALUE, rmax = Number.MIN_VALUE, maxProfit=Number.MIN_VALUE;
+        for(let i=0; i<prices.length; i++) {
+            if(i==0) {
+                left[i] = 0;
+                right[prices.length-1-i]=0;
+                lmin = prices[i];
+                rmax = prices[prices.length-1-i];
+            } else {
+                left[i] = Math.max(prices[i]-lmin, left[i-1]);
+                right[prices.length-1-i] = Math.max(rmax-prices[prices.length-1-i], right[prices.length-i]);
+                lmin = lmin > prices[i] ? prices[i]: lmin;
+                rmax = rmax < prices[prices.length-1-i] ? prices[prices.length-1-i]: rmax;
+            }
+        }
+        for(let i=0; i<prices.length-1; i++) {
+            maxProfit = Math.max(maxProfit, left[i]+right[i+1]);
+        }
+        return Math.max(maxProfit, right[0]);
+    };
+    return findWithDivideAndConquer();
+    // return findMaxProfitWithDP(0, 0, 2);
 };
