e36aaba58e803bf11459ea1043217e9fb03e6cc2
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2023-12-07 10:58:32 +0530

    Time: 86 ms (65.05%), Space: 41.5 MB (100.00%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.js
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -5,7 +5,7 @@
 // O(n^3) solution still faster
 var maxPoints = function(points) {
     let max = 1;
-    for(let i=0; i< points.length; i++) {
+    for(let i=0; i<points.length; i++) {
         for(let j=i+1; j<points.length; j++) {
             let pts = 2, dij= points[i][0]*points[j][1] - points[j][0] * points[i][1];
             for(let k = j+1; k< points.length; k++) {
