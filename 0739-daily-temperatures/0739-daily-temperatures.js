cb8e970bf195eff6b2ea00f8c0ed29033935cc32
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-02-22 21:21:35 +0530

    Time: 219 ms (52.53%), Space: 73.1 MB (50.46%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let result = new Array(temperatures.length).fill(0), stack = [];
+    for(let i=0; i<temperatures.length; i++) {
+        while(stack.length && temperatures[stack.at(-1)] < temperatures[i]) {
+            result[stack.at(-1)] = i-stack.at(-1);
+            stack.pop();
+        }
+        stack.push(i);
+    }
+    return result;
+};
