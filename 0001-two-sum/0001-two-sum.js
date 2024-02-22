fe0f03d8a4bec11a53242b519c41c43b302d7850
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-02-22 10:13:25 +0530

    Time: 56 ms (82.12%), Space: 51.2 MB (17.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let size = nums.length;
+    let map = new Map();
+    for(let i = 0; i < size; i++) {
+        let required = target - nums[i];
+        if(map.has(required)) {
+            return [map.get(required), i]
+        }
+        map.set(nums[i], i);
+    }
+};
