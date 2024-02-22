55b8ac24fad2bcbd8036d76268cfe4ef2703a0e9
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-02-22 23:45:57 +0530

    Time: 65 ms (25.40%), Space: 50.3 MB (29.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,25 +3,19 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let array = [];
+    let stack = [];
     for(let i=0; i<s.length; i++) {
-        if(s[i] === '(' || s[i] === '{' || s[i] === '[') {
-            array.push(s[i]);
-        } else if(s[i] === ')' || s[i] === '}' || s[i] === ']') {
-            if(array.length) {
-                if(
-                    (array[array.length-1] === '(' && s[i] === ')') ||
-                    (array[array.length-1] === '{' && s[i] === '}') ||
-                    (array[array.length-1] === '[' && s[i] === ']')
-                ) {
-                    array.pop();
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if(s[i] === '{' || s[i] === '[' || s[i] === '(') {
+            stack.push(s[i]);
+        } else if(stack.length && 
+            ((s[i] === '}' && stack[stack.length-1] === '{') ||
+            (s[i] === ']' && stack[stack.length-1] === '[') ||
+            (s[i] === ')' && stack[stack.length-1] === '(') )   
+        ){
+            stack.pop();
+        } else {
+            return false;
         }
     }
-    return array.length === 0;
+    return !stack.length;
 };
