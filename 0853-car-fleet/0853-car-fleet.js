2e31e1206bcc1371ed7a24646b3085769e577d82
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-02-23 09:08:38 +0530

    Time: 240 ms (46.28%), Space: 77.4 MB (53.62%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -5,19 +5,18 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    let map = new Map(), stack=[];
-    for(let i=0; i<position.length; i++) {
-        map.set(position[i], [speed[i], ((target-position[i])/speed[i])]);
+    let pairs = [];
+    for(let i=0; i< position.length; i++) {
+        pairs.push([position[i], speed[i]]);
     }
-    position.sort((a,b) => b-a);
-    for(let i=0; i<position.length; i++) {
-        let current = map.get(position[i]);
-        if(stack.length && current[1] <= map.get(stack.at(-1))[1]) {
-            map.set(position[i], [Math.min(current[0], map.get(stack.at(-1))[0]),
-                     Math.max(current[1], map.get(stack.at(-1))[1])]);
-            stack.pop();
+    pairs.sort((a,b) => a[0]-b[0]);
+    let ttr = 0, fleets = 0;
+    while(pairs.length) {
+        let car = pairs.pop();
+        if(ttr < ((target-car[0])/car[1])) {
+            ttr = ((target-car[0])/car[1]);
+            fleets++;
         }
-        stack.push(position[i]);
     }
-    return stack.length;
+    return fleets;
 };
