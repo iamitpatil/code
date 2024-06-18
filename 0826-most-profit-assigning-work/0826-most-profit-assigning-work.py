d0275e8c81b3525c938eb2fa0c0a48b05c4ea83b
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-06-18 14:19:05 +0530

    Time: 380 ms (18.12%), Space: 19.5 MB (85.19%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -6,7 +6,6 @@ class Solution:
         j = len(worker) - 1
         for i in range(0, len(difficulty)):
             heapq.heappush(heap, (-profit[i], difficulty[i]))
-        print(heap)
         while j >= 0:
             if len(heap) == 0: return total
             (p, d) = heapq.heappop(heap)
@@ -14,10 +13,4 @@ class Solution:
                 total = total+(-1*p)
                 j = j-1
         return total
-#         for i in range(0, len(difficulty)):
-#             for j in range(0, len(worker)):
-#                 if worker[j] >= difficulty[i] and profit[i] > max_profit[j]:
-#                     max_profit[j] = profit[i]
-#         return sum(max_profit)
-            
             
