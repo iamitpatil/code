aeb9347d70be09b24c0f1f28d097f5562adf8529
Amit Patil <51691510+iamitpatil@users.noreply.github.com>
2024-06-27 18:40:12 +0530

    Time: 671 ms (43.19%), Space: 52 MB (86.58%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -5,13 +5,10 @@ class Solution:
             if center is None:
                 center = {edge[0]: 1, edge[1]: 1}
                 continue
-            else:
-                if edge[0] != edge[1]:
-                    if edge[0] in center:
-                        center[edge[0]] += 1
-                    if edge[1] in center:
-                        center[edge[1]] += 1
+            else:   
+                for node in edge:
+                    if node in center:
+                        center[node] += 1
         for key in center:
             if center[key] == len(edges):
                 return key
-        return -1
